7740f4a13724d6670e8f87ea4d3e662ba0e3e278
01ihsan <karatasihsan01@gmail.com>
2022-07-18 11:00:46 +0200

    JsonPath intro

---
+++ src/test/java/com/cydeo/day4/SpartanWithJsonPath.java
@@ -0,0 +1,21 @@
+package com.cydeo.day4;
+
+import com.cydeo.Utilities.SpartanTestBase;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpartanWithJsonPath extends SpartanTestBase {
+    @Test
+    public void test1() {
+        Response response = RestAssured.given().accept(ContentType.JSON).and().pathParams("id", 5).
+                when().get("/api/spartans/{id}");
+        JsonPath jsonPath=response.jsonPath(); //jsonPath use
+        System.out.println(jsonPath.getString("name"));     //with JsonPath
+        System.out.println(response.path("name").toString()); //without JsonPath
+    }
+}
