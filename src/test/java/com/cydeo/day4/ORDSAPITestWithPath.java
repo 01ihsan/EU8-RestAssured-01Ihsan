1e2e9fe29e559db709ca1b34c1d4be6ab12e2be6
01ihsan <karatasihsan01@gmail.com>
2022-07-18 10:17:22 +0200

    ORDSAPITestWithPath Created

---
+++ src/test/java/com/cydeo/day4/ORDSAPITestWithPath.java
@@ -0,0 +1,14 @@
+package com.cydeo.day4;
+
+import com.cydeo.Utilities.HRTestBase;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ORDSAPITestWithPath extends HRTestBase {
+
+    @Test
+    public void test1() {
+
+    }
+}
