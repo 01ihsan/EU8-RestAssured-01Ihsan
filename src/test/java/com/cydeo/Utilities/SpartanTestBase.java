19ff2c81a6b622ad48a38b2032394b8a6cfc33f2
01ihsan <karatasihsan01@gmail.com>
2022-07-19 12:06:06 +0200

    Connecting Spartan database with JDBC

--- src/test/java/com/cydeo/Utilities/SpartanTestBase.java
+++ src/test/java/com/cydeo/Utilities/SpartanTestBase.java
@@ -1,5 +1,6 @@
 package com.cydeo.Utilities;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 import static io.restassured.RestAssured.baseURI;
@@ -11,5 +12,11 @@ public abstract class SpartanTestBase {
         String dbUrl = "jdbc:oracle:thin:@54.204.212.30:1521:XE";
         String dbUsername = "SP";
         String dbPassword = "SP";
+
+        DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
+    }
+    @AfterAll
+    public static void teardown(){
+        DBUtils.destroy();
     }
 }
---
+++ src/test/java/com/cydeo/day5/SpartanAPIvsDB.java
@@ -0,0 +1,14 @@
+package com.cydeo.day5;
+
+import com.cydeo.Utilities.SpartanTestBase;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+public class SpartanAPIvsDB extends SpartanTestBase {
+    @Test
+    public void test1() {
+
+    }
+}
