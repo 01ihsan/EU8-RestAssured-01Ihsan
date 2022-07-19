120107c4114eea38e3b9027732353c60685cda59
01ihsan <karatasihsan01@gmail.com>
2022-07-19 12:01:48 +0200

    Connecting Spartan database with JDBC

--- src/test/java/com/cydeo/Utilities/SpartanTestBase.java
+++ src/test/java/com/cydeo/Utilities/SpartanTestBase.java
@@ -8,5 +8,8 @@ public abstract class SpartanTestBase {
     @BeforeAll
     public static void init() {
         baseURI = "http://54.204.212.30:8000";
+        String dbUrl = "jdbc:oracle:thin:@54.204.212.30:1521:XE";
+        String dbUsername = "SP";
+        String dbPassword = "SP";
     }
 }
