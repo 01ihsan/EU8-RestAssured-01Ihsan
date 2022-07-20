6acbee053def2d243703892eaef19c852769486c
01ihsan <karatasihsan01@gmail.com>
2022-07-20 11:29:18 +0200

    ORDSPojoGetRequestTest Pojo

---
+++ src/test/java/com/cydeo/day6/ORDSPojoGetRequestTest.java
@@ -0,0 +1,11 @@
+package com.cydeo.day6;
+
+import com.cydeo.Utilities.HRTestBase;
+import org.junit.jupiter.api.Test;
+
+public class ORDSPojoGetRequestTest extends HRTestBase {
+    @Test
+    public void test1(){
+
+    }
+}
